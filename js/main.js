084ad9c4abeba405387935cc9257572f33ea8624
JamieVaughn <wxv6@case.edu>
2018-08-08 00:42:49 -0400

    Configure simpleForm handler

--- js/main.js
+++ js/main.js
@@ -75,12 +75,45 @@ pageLinks.forEach(item => item.addEventListener('click', function() {
     }
 }))
  // Form Handler
+ var form = document.querySelector('#ajax-contact');
+ var formMsg = document.querySelector('#form-feedback');
+ function getParam(param) {
+    return new URLSearchParams(window.location.search).get(param);
+}
+
+ if(getParam('submit') === 'success') {
+     formMsg.classList.remove('hidden');
+ }
+
+// add recaptcha and akismet
+
+//  form.addEventListener('submit', function(event) {
+//     event.preventDefault;
+//  })
+
  var email = document.querySelector('#email');
  var msg = document.querySelector('#message');
- var handler = () => email.style.visibility = "hidden";
- if(email.value && !email.activeElement) {
-     handler();
- }
- function postMsg() {
-     return;
- }
+
+// https://stackoverflow.com/questions/11661187/form-serialize-javascript-no-framework
+//  https://plainjs.com/javascript/ajax/serialize-form-data-into-an-array-46/
+ function serializeArray(form) {
+    var field, l, s = [];
+    if (typeof form == 'object' && form.nodeName == "FORM") {
+        var len = form.elements.length;
+        for (var i=0; i<len; i++) {
+            field = form.elements[i];
+            if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
+                if (field.type == 'select-multiple') {
+                    l = form.elements[i].options.length; 
+                    for (j=0; j<l; j++) {
+                        if(field.options[j].selected)
+                            s[s.length] = { name: field.name, value: field.options[j].value };
+                    }
+                } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+                    s[s.length] = { name: field.name, value: field.value };
+                }
+            }
+        }
+    }
+    return s;
+}
