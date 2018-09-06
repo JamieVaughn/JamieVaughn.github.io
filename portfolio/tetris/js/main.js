d6d273f0d41fa9ac0cc35d19dad7622b1e63c8b7
JamieVaughn <wxv6@case.edu>
2018-09-05 23:34:12 -0400

    Add collision detection and handling

--- portfolio/tetris/js/main.js
+++ portfolio/tetris/js/main.js
@@ -5,7 +5,7 @@ let score = 0;
 let gameOver = false;
 
 const board = {
-    row: 20,
+    row: 22,
     col: 10,
     size: 20,
     empty: 'white',
@@ -21,11 +21,6 @@ const board = {
                 drawSquare(c, index, this.empty)
             })
         })
-        // for( r = 0; r <this.row; r++){
-        //     for(c = 0; c < this.col; c++){
-        //         drawSquare(c,r,this.empty);
-        //     }
-        // }
     }
 }
 const gameBoard = board.init()
@@ -43,12 +38,15 @@ function randType() {
     return types[Math.floor(Math.random() * types.length)]
 }
 
-let p = getTetromino(randType())
-let current = initTetromino(p, {x: 5, y: 1})
+let proto = getTetromino(randType())
+let origin = {x: 5, y: 0}
+let current = initTetromino(proto, origin)
 
 function initTetromino(tetromino, pos){
     return {
+        type: tetromino.type,
         color: tetromino.color,
+        direction: 'up',
         position: tetromino.up.map(cell => {
             return [cell[0] + pos.x, cell[1] + pos.y]
         })
@@ -56,25 +54,58 @@ function initTetromino(tetromino, pos){
 }
 
 const draw = function(tetromino, flag = 0){
+    // if(didCollide(tetromino)) return;
     tetromino.position.forEach(i => {
         drawSquare([i[0]], [i[1]], flag ? 'white' : tetromino.color)
     })
     return;
 }
 function move(tetromino, nudge = 0) {
-    console.log(tetromino.position[3])
-    let increment = nudge ? [0, 0] : [0, 1]
-    increment[0] += nudge // nudge will be 0 by default, -1 for left; +1 for right
-    draw(current, 1)
-    current = {
-        color: tetromino.color,
-        position: tetromino.position.map(cell => [cell[0] + increment[0], cell[1] + increment[1]])
+    if(didCollide(tetromino) === true) {
+        didCollide(tetromino)
+        return current = initTetromino(getTetromino(randType()), origin)
+    } else {
+        let increment = nudge ? [0, 0] : [0, 1]
+        increment[0] += nudge; // nudge will be 0 by default, -1 for left; +1 for right
+        console.log(increment[0])
+        draw(tetromino, 1) // erase old tetromino
+        tetromino = {
+            type: tetromino.type,
+            color: tetromino.color,
+            direction: tetromino.direction,
+            position: tetromino.position.map(cell => [cell[0] + increment[0], cell[1] + increment[1]])
+        }
+        draw(tetromino) //draw new tetromino
+        return tetromino;
     }
-    return current
 }
 
 function rotate(tetromino) {
-    return;
+    let directions = ['up', 'right', 'down', 'left']
+    let next = directions.indexOf(tetromino.direction) + 1
+    console.log(next)
+    let rotateTo = next === 4 ? 'up' : directions[next]
+    draw(tetromino, 1)
+    tetromino.direction = rotateTo;
+    let posAdjustment = getTetromino(tetromino.type)[rotateTo]
+    tetromino.position = posAdjustment.map(i => [tetromino.position[2][0]+i[0], tetromino.position[2][1]+i[1]])
+    draw(tetromino)
+    return tetromino;
+}
+
+function didCollide(tetromino) {
+    let sliced = tetromino.position.filter(i=> i[0]<=0 )
+    let hooked = tetromino.position.filter(i=> i[0]>=9)
+    let floored = tetromino.position.filter(i=> i[1]>=21)
+    console.log(sliced, hooked, floored)
+    if (sliced.length > 0) {
+        return 'sliced'
+    } else if (hooked.length > 0) {
+        return 'hooked'
+    } else if (floored.length > 0) {
+        return true
+    }
+    return false;
 }
 
 draw(current)
@@ -86,9 +117,7 @@ function driftIO() {
     if(gameOver) { return }
     if((now - start) > 1000) {
         start = Date.now()
-        move(current)
-        draw(current)
-        
+        current = move(current)
     }
     requestAnimationFrame(driftIO)
 }
@@ -96,16 +125,19 @@ function driftIO() {
 driftIO()
 
 
-document.addEventListener('keydown', function(e){
-    if(e.which === 37) {
-        move(current, -1)
-        draw(current)
-    } else if (e.which === 39) {
-        move(current, 1)
-        draw(current)
+document.addEventListener('keydown', function(e){    
+    let negate = didCollide(current) // negate nudge if tetromino didCollide
+    if(e.which === 37 && negate !== 'sliced') {
+        current = move(current, -1)
+        
+    } else if (e.which === 39 && negate !== 'hooked') {
+        current = move(current, 1)
+       
     } else if (e.which === 38) {
-        rotate(current)
-        draw(current)
+        current = rotate(current)
+       
+    } else if (e.which === 40) {
+        current = move(current)
+        
     }
-
 })
