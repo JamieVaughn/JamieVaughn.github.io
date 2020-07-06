6ced9893ad87b7063cf7335b6669c2836fed6cee
JamieVaughn <wxv6@case.edu>
2020-07-06 02:57:27 -0400

    Refactor snake game and add collisions, features

--- games/snake/app.js
+++ games/snake/app.js
@@ -1,118 +1,95 @@
-const loot = ['🍞', '🥖', '🥪', '🥐', '🥯', '🥟', '🥨', '🥞', '🧇']
-const enemy = ['🦡', '🦅', '🐒', '🦉', '🐊']
 // Board setup
-var timer = 350;
-var edge = 14;
-var boardSize = edge*edge;
-var array = [].fill.call({ length: boardSize }, 1);
-var htmlElements = "";
-for (let i = 0; i < array.length; i++) {
-   htmlElements += `<div class="cell" id="${i}" data-icon="${loot[Math.floor(Math.random() * loot.length)]}"></div>`;
+const state = {
+    timer: 350,
+    interval: undefined,
+    edge: 14,
+    snake: [37, 38],
+    direction: 'left',
+    breadLocation: undefined,
+    loot: ['🍞', '🥖', '🥪', '🥐', '🥯', '🥟', '🥨', '🥞', '🧇'],
+    enemies: ['🦡', '🦅', '🐒', '🦉', '🐊']
 }
-document.getElementById("container").innerHTML = htmlElements;
+const grid = document.getElementById("container")
+function setupBoard(dimension) { // dimension is length of one side of square grid
+    [...Array(dimension**2).keys()].forEach(i => {
+        grid.innerHTML += `
+        <div class="cell" id="${i}" 
+        data-icon="${state.loot[Math.floor(Math.random() * state.loot.length)]}">
+        </div>`;
+    })
+    state.breadLocation = bread(rand([37, 38]));
+}
+function initClock (tick) {
+    state.timer = tick
+    clearInterval(state.interval)
+    state.interval = setInterval(update, tick)
+}
+setupBoard(state.edge)
 //buttons
-var play = document.getElementById("play");
-  play.addEventListener("click", function(){
-  timer = 350;
-  clearInterval(interval)
-  interval  = setInterval(update, timer);
-})
-document.getElementById("pause").addEventListener("click", function(){
-  timer = 9999999999999;
-  clearInterval(interval)
-  interval  = setInterval(update, timer);
-  console.log(timer)
-})
-document.getElementById("reset").addEventListener("click", function(){
-  console.log("reload");
-  document.querySelectorAll('.cell').forEach(c => c.classList.remove('snake'))
-  reload();
-})
+document.getElementById("play").addEventListener("click", () => initClock(350))
+document.getElementById("pause").addEventListener("click", () => initClock(9999999999))
+document.getElementById("reset").addEventListener("click", reload)
 function reload() {
-    snake = [27, 28];
-    len = snake.length-1;
-    snakeDirection = "left";
-    head = undefined;
-    clearInterval(interval)
-    timer = 350
-    interval  = setInterval(update, timer);
-
-}
-//snake setup
-var snake = [27, 28];
-var len = snake.length-1;
-var snakeDirection = "left";
-var head;
-
-var rand = function(size, arr) {
-   let num = Math.floor(Math.random() * size);
-  while (arr.includes(num)){
-    num = Math.floor(Math.random() * size);
-  }
-  return num;
-}
-function bread (cell) {
-  document.getElementById(cell.toString()).classList.add("bread")
-  return cell;
-}
-var breadLocation = bread(rand(boardSize, snake));
-var eat = function (head, appl) {
-  if(head.toString() == appl){
-    document.getElementById(appl).classList.remove("bread");
-    snake.push(snake[snake.length-1]);
-    return bread(rand(boardSize, snake));
-  }
-  return appl;
-}
-
-function slither() {
-  head = snake[0];
-  if(snakeDirection=="right"){
-    document.getElementById(snake.pop()).classList.remove("snake");
-    snake.unshift(head+1);
-  }
-  if(snakeDirection=="left"){
-    document.getElementById(snake.pop()).classList.remove("snake");
-    snake.unshift(head-1);
-  }
-  if(snakeDirection=="up"){
-    document.getElementById(snake.pop()).classList.remove("snake");
-    snake.unshift(head-edge);
-  }
-  if(snakeDirection=="down"){
-    document.getElementById(snake.pop()).classList.remove("snake");
-    snake.unshift(head+edge);
-  }
-  for(let i = 0; i<snake.length; i++) {
-    document.getElementById(snake[i]).classList.add("snake");
-  }
-  return head;
-}
-function checkCollision(pos, size, arr){
-  
-  if(pos === "undefined" || pos < 0 || pos > size || arr.slice(1, arr.length-1).includes(pos)) {
-    console.log("loss condition detected.");
-    document.getElementById("container").innerHTML = "<h1> Game Over </h1>";
-  }
+    grid.innerHTML = ''
+    setupBoard(state.edge)
+    state.snake = [37, 38];
+    state.direction = "left";
+    initClock(350)
 }
 document.onkeydown = function(e) {
   switch (e.keyCode) {
-    case 37: if(snakeDirection!="right"){snakeDirection = "left" }; break;
-    case 38: if(snakeDirection!="down"){snakeDirection = "up"}; break;
-    case 39: if(snakeDirection!="left"){snakeDirection = "right"}; break;
-    case 40: if(snakeDirection!="up"){snakeDirection = "down"}; break;
-    }
+    case 37: if(state.direction!="right"){state.direction = "left" }; break;
+    case 38: if(state.direction!="down"){state.direction = "up"}; break;
+    case 39: if(state.direction!="left"){state.direction = "right"}; break;
+    case 40: if(state.direction!="up"){state.direction = "down"}; break;
+  }
 };
-
-//update loop
-// while (playing) {
-function update () {
-  var position = slither();
-  console.log(snake, position, snake[0], boardSize);
-  
-  breadLocation = eat(position, breadLocation);
-  checkCollision(snake[0], boardSize, snake);
-  
+//utils
+function rand(arr) {
+    let num;
+    do { num = Math.floor(Math.random() * (state.edge**2));
+    } while (arr.includes(num))
+  return num;
 }
-var interval = setInterval(update, timer);
-// }
+function bread (cell) {
+  document.getElementById(cell).classList.add("bread")
+  return cell;
+}
+var eat = function (tail, breadPos) {
+    document.getElementById(breadPos).classList.remove("bread");
+    state.snake.push(tail);
+    return bread(rand(state.snake));
+}
+function slither(dir, h) {
+  document.getElementById(state.snake.pop())?.classList.remove("snake");
+  let table = {
+    right: (h) => state.snake.unshift(h+1),
+    left: (h) => state.snake.unshift(h-1),
+    up: (h) => state.snake.unshift(h-state.edge),
+    down: (h) => state.snake.unshift(h+state.edge),
+  }
+  table[dir](h)
+  state.snake.forEach(s => document.getElementById(s)?.classList.add("snake"))
+}
+function checkCollision(pos, size, arr){
+  let selfCollision = arr.slice(1, arr.length-1).includes(pos)
+  let vertCollision = pos === undefined || pos < 0 || pos > size 
+  let lateralCollision = arr[1]%14 === 0 && (pos+1)%14 === 0 || (arr[1]+1)%14 === 0 && pos%14 === 0
+  if( lateralCollision || vertCollision || selfCollision) {
+    gameOver(selfCollision)
+  }
+}
+function gameOver(cause) {
+    document.getElementById("container").innerHTML = `
+        <h1> Game Over </h1><h5>You collided with ${cause ? 'yourself' : 'the edge'}`;
+    clearInterval(state.interval)
+}
+//update loop
+function update () {
+    let head = state.snake[0];
+    let tail = state.snake[state.snake.length - 1]
+    checkCollision(head, state.edge**2, state.snake);
+    slither(state.direction, head)
+    if(head == state.breadLocation) state.breadLocation = eat(tail, state.breadLocation);
+}
+state.interval = setInterval(update, state.timer);
