7ec94ed5f9f3aa352c74da24ebc2a01cc876d23a
JamieVaughn <wxv6@case.edu>
2024-03-10 03:16:03 -0400

    Adjust kanban, fix transition issue

--- portfolio/kanban/drag.js
+++ portfolio/kanban/drag.js
@@ -3,22 +3,39 @@ const droppables = document.querySelectorAll('.lane');
 const allowSortDrop = true
 
 draggables.forEach(task => {
-  task.addEventListener('dragstart', () => {
+  task.addEventListener('dragstart', e => {
     task.classList.add('dragging');
-    })
-    task.addEventListener('dragend', () => {
-      task.classList.remove('dragging');
-    })
+    // e.dataTransfer.setDragImage(document.querySelector('button'), '50%', '50%') // make the drag "ghost" appear like another element
+  })
+  task.addEventListener('dragend', () => {
+    task.classList.remove('dragging');
+  })
 })
 
 droppables.forEach(lane => {
   lane.addEventListener('dragover', e => {
     e.preventDefault();
+    lane.classList.add('dragover');
     const afterElement = getDragAfterElement(lane, e.clientY);
     const draggable = document.querySelector('.dragging');
     allowSortDrop && afterElement
       ? lane.insertBefore(draggable, afterElement)
       : lane.appendChild(draggable)
+  })
+})
+// droppables.forEach(lane => {
+//   lane.addEventListener('dragenter', e => {
+//   })
+// })
+droppables.forEach(lane => {
+  lane.addEventListener('dragleave', e => {
+    e.stopPropagation()
+    lane.classList.remove('dragover');
+  })
+})
+droppables.forEach(lane => {
+  lane.addEventListener('dragend', e => {
+    lane.classList.remove('dragover');
   })
 })
 
