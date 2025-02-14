9324718947de0dfef71f91d9818eefd4c309671b
JamieVaughn <wxv6@case.edu>
2025-02-14 00:47:05 -0500

    format quotes file

--- portfolio/commonplace/quotes.js
+++ portfolio/commonplace/quotes.js
@@ -1,143 +1,235 @@
 export const quotes = [
-  {quote: "What you leave behind is not what is engraved in stone monuments, but what is woven into the lives of others.", authoer:"Pericles"},
-  {quote: "Education consists mainly of what we have unlearned.", author: "Mark Twain"},
-  {quote: "I've never let my schooling interfere with my education.", author: "Mark Twain"},
-  {quote: "There is no such thing as a tree. No such thing as a fish. A tree is a strategy. Wood is a strategy. Fruit is a strategy. A fish is also a strategy.", author: "Eukaryote Writes Blog"},
-  {quote: "A complex system that works is invariably found to have evolved from a simple system that worked. A complex system designed from scratch never works and cannot be patched up to make it work. You have to start over with a working simple system.", author: "John Gall (Systemantics - 1975)"},
-  {quote: "I hate quotations. Tell me what YOU know.", author: "Ralph Waldo Emerson"},
-  {quote: "There is neither good, nor bad, but thinking makes it so.", author: "Hamlet"},
-  {quote: "It is the arrogance of every age to believe that yesterday was calm.", author: "Tom Peters"},
-  {quote: "Inquire within.", author: "Anonymous"},
-  {quote: "Community precedes commerce.", author: "John Hagel"},
-  {quote: "To measure is to know.", author: "Lord Kelvin"},
-  {quote: "There is much ruin in a nation.", author: "Adam Smith"},
-  {quote: "Waste flows downstream. But we all live downstream too.", author: "Anonymous"},
-  {quote: "Some people become vandals in order to make the world a better looking place.", author: "Banksy"},
-  {quote: "Strength is the outcome of need.", author: "H.G. Wells"},
-  {quote: "Our ignorance is not as vast as our failure to use what we know.", author: "M. King Hubbert"},
-  {quote: "We are doomed to make choices. This is life's greatest paradox.", author: "Wayne Dyer"},
-  {quote: "We do not see things as they are. We see things as we are.", author: "Talmud"},
-  {quote: "Life is change. Growth is optional. Choose wisely.", author: "Karen Kaiser Clark"},
-  {quote: "If you want to get somewhere fast, go alone. If you want to get far, go together.", author: "African proverb"},
-  {quote: "Man can do what he wants but he cannot want what he wants.", author: "Arthur Schopenhauer"},
-  {quote: "As soil goes, so goes civilizaiton.", author: "Woody Tasch"},
-  {quote: "There are two ways to be rich: one is by acquiring much, the other is by desiring little.", author: "Jackie French Koller"},
-  {quote: "Think about what must go right, instead of what could possibly go wrong.", author: "Leslie Lamport"},
-  {quote: "We fail more often because we solve the wrong problem than because we get the wrong solution to the right problem.", author: "Russell L. Ackoff"},
-  {quote: "It isn't that they cannot find the solution. It is that they cannot see the problem.", author: "G.K Chesterton"},
-  {quote: "If you are unable to understand the cause of a problem, it is impossible to solve it.", author: "Naoto Kan"},
-  {quote: "We shape our tools, and thereafter our tools shape us.", author: "Marshall McLuhan"},
   {
-    quote: "Life is not a maintenance or restoration of equilibrium but is essentially the maintenance of diequilibria, as the doctrine of the organism as open system reveals. Reaching equilibrium means death and consequent decay.",
+    quote: "What you leave behind is not what is engraved in stone monuments, but what is woven into the lives of others.",
+    author: "Pericles",
+  },
+  {
+    quote: "Education consists mainly of what we have unlearned.",
+    author: "Mark Twain",
+  },
+  {
+    quote: "I've never let my schooling interfere with my education.",
+    author: "Mark Twain",
+  },
+  {
+    quote:
+      "There is no such thing as a tree. No such thing as a fish. A tree is a strategy. Wood is a strategy. Fruit is a strategy. A fish is also a strategy.",
+    author: "Eukaryote Writes Blog",
+  },
+  {
+    quote:
+      "A complex system that works is invariably found to have evolved from a simple system that worked. A complex system designed from scratch never works and cannot be patched up to make it work. You have to start over with a working simple system.",
+    author: "John Gall (Systemantics - 1975)",
+  },
+  {
+    quote: "I hate quotations. Tell me what YOU know.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    quote: "There is neither good, nor bad, but thinking makes it so.",
+    author: "Hamlet",
+  },
+  {
+    quote: "It is the arrogance of every age to believe that yesterday was calm.",
+    author: "Tom Peters",
+  },
+  { quote: "Inquire within.", author: "Anonymous" },
+  { quote: "Community precedes commerce.", author: "John Hagel" },
+  { quote: "To measure is to know.", author: "Lord Kelvin" },
+  { quote: "There is much ruin in a nation.", author: "Adam Smith" },
+  {
+    quote: "Waste flows downstream. But we all live downstream too.",
+    author: "Anonymous",
+  },
+  {
+    quote: "Some people become vandals in order to make the world a better looking place.",
+    author: "Banksy",
+  },
+  { quote: "Strength is the outcome of need.", author: "H.G. Wells" },
+  {
+    quote: "Our ignorance is not as vast as our failure to use what we know.",
+    author: "M. King Hubbert",
+  },
+  {
+    quote: "We are doomed to make choices. This is life's greatest paradox.",
+    author: "Wayne Dyer",
+  },
+  {
+    quote: "We do not see things as they are. We see things as we are.",
+    author: "Talmud",
+  },
+  {
+    quote: "Life is change. Growth is optional. Choose wisely.",
+    author: "Karen Kaiser Clark",
+  },
+  {
+    quote:
+      "If you want to get somewhere fast, go alone. If you want to get far, go together.",
+    author: "African proverb",
+  },
+  {
+    quote: "Man can do what he wants but he cannot want what he wants.",
+    author: "Arthur Schopenhauer",
+  },
+  { quote: "As soil goes, so goes civilizaiton.", author: "Woody Tasch" },
+  {
+    quote:
+      "There are two ways to be rich: one is by acquiring much, the other is by desiring little.",
+    author: "Jackie French Koller",
+  },
+  {
+    quote:
+      "Think about what must go right, instead of what could possibly go wrong.",
+    author: "Leslie Lamport",
+  },
+  {
+    quote:
+      "We fail more often because we solve the wrong problem than because we get the wrong solution to the right problem.",
+    author: "Russell L. Ackoff",
+  },
+  {
+    quote:
+      "It isn't that they cannot find the solution. It is that they cannot see the problem.",
+    author: "G.K Chesterton",
+  },
+  {
+    quote:
+      "If you are unable to understand the cause of a problem, it is impossible to solve it.",
+    author: "Naoto Kan",
+  },
+  {
+    quote: "We shape our tools, and thereafter our tools shape us.",
+    author: "Marshall McLuhan",
+  },
+  {
+    quote:
+      "Life is not a maintenance or restoration of equilibrium but is essentially the maintenance of diequilibria, as the doctrine of the organism as open system reveals. Reaching equilibrium means death and consequent decay.",
     author: "Ludwig von Bertalanffy",
-    annotation: "Maybe the concept of Buddhist Nirvana is not reaching an equilibrium, but rather a steady state, so as to extract oneself from being buffetted by dynamical ebbs and flows, which are the more common form that disequilibria will naturally take. So, while equilibrium means death, disequilibria may be occupied in two formats: the chaos of dynamical system and the relative calmness of steady state."
+    annotation:
+      "Maybe the concept of Buddhist Nirvana is not reaching an equilibrium, but rather a steady state, so as to extract oneself from being buffetted by dynamical ebbs and flows, which are the more common form that disequilibria will naturally take. So, while equilibrium means death, disequilibria may be occupied in two formats: the chaos of dynamical system and the relative calmness of steady state.",
   },
   {
     quote: "Be ruthless to systems, but be kind to people.",
     author: "Michael Brooks",
   },
   {
-    quote: "You will continue to suffer if you have an emotional reaction to everything that is said to you. True power is sitting back and observing things with logic. True power is restraint. If words control you that means everyone else can control you. Breathe and allow things to pass.",
+    quote:
+      "You will continue to suffer if you have an emotional reaction to everything that is said to you. True power is sitting back and observing things with logic. True power is restraint. If words control you that means everyone else can control you. Breathe and allow things to pass.",
     author: "Warren Buffet",
   },
   {
-    quote: "Not only is it easy to lie with maps, it's essential... To avoid hiding critical information in a fog of detail, the map must offer a selective, incomplete view of reality.",
+    quote:
+      "Not only is it easy to lie with maps, it's essential... To avoid hiding critical information in a fog of detail, the map must offer a selective, incomplete view of reality.",
     author: "Mark Monmonier - How to lie with maps",
   },
   {
-    quote: "90% of what I say in public I’ve written down before. Whenever I say something that sounds elegant, it’s just because I’ve written it. I’ve structured the ideas, I’ve done the hard work.",
-    author: "Neil DeGrasse Tyson"
+    quote:
+      "90% of what I say in public I’ve written down before. Whenever I say something that sounds elegant, it’s just because I’ve written it. I’ve structured the ideas, I’ve done the hard work.",
+    author: "Neil DeGrasse Tyson",
   },
   {
     quote: "There are no such thing as rules, only consequences.",
-    author: "Brian S. Glick, CEO, Chain.io"
+    author: "Brian S. Glick, CEO, Chain.io",
   },
   {
-    quote: "Humility is not about thinking less of yourself, but rather thinking of yourself less.",
-    author: "C.S. Lewis (apocryphal)"
+    quote:
+      "Humility is not about thinking less of yourself, but rather thinking of yourself less.",
+    author: "C.S. Lewis (apocryphal)",
   },
   {
-    quote: "A toolmaker succeds as, and only as, the users of his tool succeed with his aid. However shiny the blade, however jeweled thee hilt, however perfect the heft, a sword is tested only by cutting. That sword smith is successful whose clients die of old age.",
-    author: "Frederick Brooks (Computer Scientist As Toolsmith)"
+    quote:
+      "A toolmaker succeds as, and only as, the users of his tool succeed with his aid. However shiny the blade, however jeweled thee hilt, however perfect the heft, a sword is tested only by cutting. That sword smith is successful whose clients die of old age.",
+    author: "Frederick Brooks (Computer Scientist As Toolsmith)",
   },
   {
-    quote: "It is through logic that we prove, but through intuition that we discover.",
-    author: "Henri Poincaré"
+    quote:
+      "It is through logic that we prove, but through intuition that we discover.",
+    author: "Henri Poincaré",
   },
   {
-    quote: "I’m a comedian, so I’ll speak about justice, no matter how hard it is on your ears.",
-    author: "Aristophanes"
+    quote:
+      "I’m a comedian, so I’ll speak about justice, no matter how hard it is on your ears.",
+    author: "Aristophanes",
   },
   {
-    quote: "If you try to change it, you will ruin it. If you try to hold it, you will lose it.",
-    author: "Lao Tzu, Tao Te Ching, 29"
+    quote:
+      "If you try to change it, you will ruin it. If you try to hold it, you will lose it.",
+    author: "Lao Tzu, Tao Te Ching, 29",
   },
   {
-    quote: "In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.",
-    author: "Douglas Adams, Hitchhiker's Guide to the Galaxy"
+    quote:
+      "In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.",
+    author: "Douglas Adams, Hitchhiker's Guide to the Galaxy",
   },
   {
     quote: "Economics is too important to leave to the economists.",
-    author: "Steve Keen"
+    author: "Steve Keen",
   },
   {
-    quote: "If economists wished to study the horse, they wouldn't go and look at horses. They'd sit in their studies and say to themselves, 'What would I do if I were a horse?'",
-    author: "Ely Devons"
+    quote:
+      "If economists wished to study the horse, they wouldn't go and look at horses. They'd sit in their studies and say to themselves, 'What would I do if I were a horse?'",
+    author: "Ely Devons",
   },
   {
-    quote: "The first principle is that you must not fool yourself — and you are the easiest person to fool.",
-    author: "Richard Feynman"
+    quote:
+      "The first principle is that you must not fool yourself — and you are the easiest person to fool.",
+    author: "Richard Feynman",
   },
   {
     quote: "I like to pay taxes. With them, I buy civilization.",
-    author: "Oliver Wendell Homes Jr."
+    author: "Oliver Wendell Homes Jr.",
   },
   {
-    quote: "The universe is not only queerer than we suppose, but queerer than we can suppose. I have read and heard many attempts at a systematic account of it... I have always felt that they were much too simple. I suspect that there are more things in heaven and earth than are dreamed of, or can be dreamed of, in any philosophy.",
-    author: "J. B. S. Haldane, 'Possible Worlds' (1927)"
+    quote:
+      "The universe is not only queerer than we suppose, but queerer than we can suppose. I have read and heard many attempts at a systematic account of it... I have always felt that they were much too simple. I suspect that there are more things in heaven and earth than are dreamed of, or can be dreamed of, in any philosophy.",
+    author: "J. B. S. Haldane, 'Possible Worlds' (1927)",
   },
   {
-    quote: "Hope and goodness is a direct rebuttal to the brutality of the world.",
-    author: "Susan Kaye Quinn"
+    quote:
+      "Hope and goodness is a direct rebuttal to the brutality of the world.",
+    author: "Susan Kaye Quinn",
   },
   {
-    quote: "Hope is definitely not the same thing as optimism. It is not the conviction that something will turn out well, but the certainty that something is worth doing, regardless of how it turns out.",
-    author: "Václav Havel"
+    quote:
+      "Hope is definitely not the same thing as optimism. It is not the conviction that something will turn out well, but the certainty that something is worth doing, regardless of how it turns out.",
+    author: "Václav Havel",
   },
   {
-    quote: "Words bend our thinking to infinite paths of self-delusion. And the fact that we spend most of our mental lives in brain mansions built of words means that we lack the objectivity necessary to see the terrible distortion of reality which language brings.",
-    author: "Dan Simmons, Hyperion"
+    quote:
+      "Words bend our thinking to infinite paths of self-delusion. And the fact that we spend most of our mental lives in brain mansions built of words means that we lack the objectivity necessary to see the terrible distortion of reality which language brings.",
+    author: "Dan Simmons, Hyperion",
   },
   {
-    quote: "Between stimulus and response, there is a space. In that space is our power to choose our response. In our response lies our growth and our freedom.",
-    author: "Victor Frankl"
-  }
-]
+    quote:
+      "Between stimulus and response, there is a space. In that space is our power to choose our response. In our response lies our growth and our freedom.",
+    author: "Victor Frankl",
+  },
+];
 
 export const colors = [
-  '#16a085',
-  '#27ae60',
-  '#2c3e50',
-  '#f39c12',
-  '#e74c3c',
-  '#9b59b6',
-  '#FB6964',
-  '#472E32',
-  '#BDBB99',
-  '#77B1A9',
-  '#73A857',
-  'crimson',
-  'dodgerblue',
-  'gold',
-  'turquoise',
-  'hotpink',
-  'rebeccapurple',
-  'seagreen',
-  'lightseagreen',
-  'cornflowerblue',
-  'slateblue',
-  'tomato',
-  'orangered',
-  'darkorange',
-  'plum'
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857",
+  "crimson",
+  "dodgerblue",
+  "gold",
+  "turquoise",
+  "hotpink",
+  "rebeccapurple",
+  "seagreen",
+  "lightseagreen",
+  "cornflowerblue",
+  "slateblue",
+  "tomato",
+  "orangered",
+  "darkorange",
+  "plum",
 ];
